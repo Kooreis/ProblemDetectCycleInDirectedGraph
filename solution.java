4892bf79d3cf45ba8f9b7cc395089b147e9a32c9
Kooreis <kooreis.official@gmail.com>
2024-11-20 20:50:33 -0600

    This part of the script defines the addEdge method, which adds an edge to the graph by adding a vertex to the adjacency list of another vertex.

--- solution.java
+++ solution.java
@@ -1,12 +1,3 @@
-import java.util.*;
-
-public class Main {
-    private static int V;
-    private static LinkedList<Integer> adj[];
-
-    Main(int v) {
-        V = v;
-        adj = new LinkedList[v];
-        for (int i=0; i<v; ++i)
-            adj[i] = new LinkedList();
+void addEdge(int v,int w) {
+        adj[v].add(w);
     }
